fe021b67ae71502163216878400fe334a95e09a5
varun <varunvarneker06@gmail.com>
2026-02-03 14:31:32 +0530

    day 7

---
+++ Arrays/FindElement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class FindElement {
+    public static void main(String[] args) {
+    Scanner z = new Scanner(System.in);
+    int n = z.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0; i<n; i++){
+        arr[i]=z.nextInt();
+    }
+    int target = z.nextInt();
+    for(int i = 0; i<n;i++){
+        if(arr[i]==target){
+            System.out.print("Target element present in "+i);
+        }
+    }
+    }
+}

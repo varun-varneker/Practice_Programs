b4f0a74400e5c25a9b63c47017b081843a34a8b3
varun <varunvarneker06@gmail.com>
2026-02-23 13:51:10 +0530

    New Code

---
+++ Arrays/ReverseStringKtimes.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void reverse(int[] arr, int l, int r){
+      while(l<r){
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        l++;
+        r--;
+      }
+    }
+    public static void main(String[] args) {
+      Scanner z = new Scanner(System.in);
+      int n = z.nextInt();
+      int[] arr = new int[n];
+      for(int i = 0; i<n; i++){
+        arr[i] = z.nextInt();
+      }
+      int k = z.nextInt();
+      
+      reverse(arr, 0, k-1);
+      
+      reverse(arr, k, n-1);
+      
+      reverse(arr, 0, n-1);
+      
+      System.out.print(Arrays.toString(arr));
+    }
+    
+}
